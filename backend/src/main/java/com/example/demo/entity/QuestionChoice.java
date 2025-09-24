44681e021153085a5c5ef45473f4b349fa0853ed
Eric <ericfrancis1@proton.me>
2025-09-24 16:12:45 -0500

    reorganize Quiz-related classes

---
+++ backend/src/main/java/com/example/demo/entity/QuestionChoice.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+public class QuestionChoice {
+
+    private String text;
+    private int points;
+    private int resultIndex;
+
+    public QuestionChoice() {
+    }
+
+    public QuestionChoice(String text, int points, int resultIndex) {
+        this.text = text;
+        this.points = points;
+        this.resultIndex = resultIndex;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getResultIndex() {
+        return resultIndex;
+    }
+
+    public void setResultIndex(int resultIndex) {
+        this.resultIndex = resultIndex;
+    }
+
+}
---
+++ backend/src/main/java/com/example/demo/entity/QuizResult.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+public class QuizResult {
+
+    private String name;
+    private String description;
+
+    public QuizResult() {
+    }
+
+    public QuizResult(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
